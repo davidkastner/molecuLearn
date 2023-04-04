43e794f7ccfed31fb0c2be1ca693e78c1eaddf2e
David Kastner <kastner@mit.edu>
2023-04-04 09:05:15 -0400

    Read PDB trajectory functionality

--- ml/process.py
+++ ml/process.py
@@ -1,29 +1,28 @@
 """Process ab-initio molecular dynamics simulations."""
 
+import pandas as pd
+from Bio.PDB import PDBParser, calc_mass, calc_center_of_mass
+from itertools import combinations
 
-def canvas(with_attribution=True):
+
+def read_trajectory_pdb(file_path):
     """
-    Placeholder function to show example docstring (NumPy format).
-
-    Replace this function and doc string for your own project.
-
+    Read a trajectory PDB file and separate it into individual frames.
+    
     Parameters
     ----------
-    with_attribution : bool, Optional, default: True
-        Set whether or not to display who the quote is from.
-
+    file_path : str
+        Path to the PDB trajectory file.
+    
     Returns
     -------
-    quote : str
-        Compiled string including quote and optional attribution.
+    list
+        A list of PDB structures representing individual frames.
+
     """
-
-    quote = "The code is but a canvas to our imagination."
-    if with_attribution:
-        quote += "\n\t- Adapted from Henry David Thoreau"
-    return quote
-
-
-if __name__ == "__main__":
-    # Do something if this file is invoked on its own
-    print(canvas())
+    with open(file_path, 'r') as f:
+        content = f.read()
+    pdb_strings = content.split("END")
+    parser = PDBParser()
+    frames = [parser.get_structure("Frame", pdb_string) for pdb_string in pdb_strings if pdb_string.strip()]
+    return frames
